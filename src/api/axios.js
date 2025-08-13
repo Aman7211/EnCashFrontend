dd2b9e2290684cc8583fe746c97eae594d8f989e
Aman7211 <109971534+Aman7211@users.noreply.github.com>
2025-08-13 08:16:13 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://encashbackend.onrender.com/',
+  baseURL: 'https://encashbackend.onrender.com/api',
 });
 
 instance.interceptors.request.use((config) => {
